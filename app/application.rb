f7b2faa69c2c38a992c6cb33a5f953ade0cb1ee8
Evan Candler <emcandler21@gmail.com>
2018-04-05 21:00:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if datetime > 12
+    if Time > 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
