c2cf24b3370752b2db82e3d025ac5ccf17a30c3d
Evan Candler <emcandler21@gmail.com>
2018-04-05 21:06:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Kernel.rand(1..24)
 
-    if Time.now < 12
+    if Time.now.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
